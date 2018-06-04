1e2e2992e812ecd9af7c1d3c2004c8013e5d1040
Shahan Krakirian <shahankrakirian@gmail.com>
2018-06-04 12:11:50 -0700

    Added Pythagorean Theorem File (Utilizing Modularized Test Files)

---
+++ fundamentals/PythagoreanTheorem/Pythagorean.java
@@ -0,0 +1,8 @@
+import java.lang.Math;
+public class Pythagorean {
+    public String calculateHypotenuse(int legA, int legB){ 
+        double sumsquares = (legA * legA) + (legB * legB);
+        double hypot = Math.sqrt(sumsquares);
+        return "Hypotenuse: " + Integer.toString(hypot);
+    }
+}
